ffa0281874ae0bd844ae312e73ce5782a559ed92
martinchichi <martincicinelli@gmail.com>
2025-02-10 20:49:13 -0300

    agregado pdf

--- temperature-api/src/presentation/routes/temperatureRoutes.js
+++ temperature-api/src/presentation/routes/temperatureRoutes.js
@@ -4,7 +4,7 @@ import { TemperatureController } from '../controllers/temperatureController.js';
 const router = express.Router();
 const controller = new TemperatureController();
 
-router.post('/temperatures', controller.create);
-router.get('/temperatures', controller.list);
+router.post('/temperatures', (req, res) => controller.create(req, res));
+router.get('/temperatures', (req, res) => controller.list(req, res));
 
 export { router as temperatureRoutes };
